2244e51b26c864f188fa60a3502d2f264e674a2d
Phil Calçado <pcalcado@gmail.com>
2011-06-01 15:43:54 -0400

    sync

--- pcalcado.el
+++ pcalcado.el
@@ -1,7 +1,11 @@
 ;;color
 
+(add-to-list 'load-path "./emacs-color-theme-solarized")
+(require 'color-theme-solarized)
+(enable-theme 'solarized-dark)
 (require 'color-theme)
 (color-theme-blackboard)
+
 
 (global-linum-mode 1)
 (setq column-number-mode t)
@@ -35,3 +39,17 @@
 ;;c#
 (require 'csharp-mode)
 (setq auto-mode-alist (cons '("\\.cs$" . csharp-mode) auto-mode-alist))
+
+;;scala
+(add-to-list 'load-path "./scala-mode")
+(require 'scala-mode)
+(setq auto-mode-alist (cons '("\\.scala$" . scala-mode) auto-mode-alist))
+;; Load the ensime lisp code...
+(add-to-list 'load-path "./ensime")
+(require 'ensime)
+
+;; This step causes the ensime-mode to be started whenever
+;; scala-mode is started for a buffer. You may have to customize this step
+;; if you're not using the standard scala mode.
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+
